b542b02a0834f274e2c79997ffee4c0232065a60
Matt Wilkinson <mytematt@gmail.com>
2018-08-22 19:49:07 -0600

    committing for safety

--- main.js
+++ main.js
@@ -2,6 +2,8 @@ document.addEventListener("DOMContentLoaded", function(){
   //variables
   var name = document.getElementById("name")
   var startButton = document.getElementById("start-button")
+  var player
+  var top = document.getElementById("top")
 
   name.addEventListener("keyup", function(event){
     if (event.target.value !== "") {
@@ -15,4 +17,15 @@ document.addEventListener("DOMContentLoaded", function(){
 
 
   //functions
+
+  function startGame() {
+    player = name.value
+    startButton.className = "hide"
+    var label = document.createElement('p')
+    label.innerHTML = 'Welcome ' + player
+    label.className = "center"
+    top.append(label)
+  }
+
+  startButton.addEventListener("click", startGame)
 })
