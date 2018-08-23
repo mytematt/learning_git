c0e8f02fb66d2db03a654e8179ba59d358379f01
Matt Wilkinson <mytematt@gmail.com>
2018-08-22 20:04:26 -0600

    stuff

--- main.js
+++ main.js
@@ -4,6 +4,10 @@ document.addEventListener("DOMContentLoaded", function(){
   var startButton = document.getElementById("start-button")
   var player
   var top = document.getElementById("top")
+  var gameArea = document.getElementById("game-area")
+  var numGuesses = 0
+  var correctGuesses = 0
+  var ballPosition, userGuess
 
   name.addEventListener("keyup", function(event){
     if (event.target.value !== "") {
@@ -18,6 +22,32 @@ document.addEventListener("DOMContentLoaded", function(){
 
   //functions
 
+function genBallPosition(){
+
+}
+
+function incCounters(){
+
+}
+
+function calcAverage(){
+
+}
+
+function makeGuess(){
+  genBallPosition()
+}
+
+  function drawBoxes() {
+    for (var i=0; i<3, i++) {
+      var box = document.createElement("div")
+      box.className = "box"
+      box.id="box-" + i
+      gameArea.append(box)
+      box.addEventListener("click", makeGuess)
+    }
+  }
+
   function startGame() {
     player = name.value
     startButton.className = "hide"
@@ -25,6 +55,7 @@ document.addEventListener("DOMContentLoaded", function(){
     label.innerHTML = 'Welcome ' + player
     label.className = "center"
     top.append(label)
+    drawBoxes()
   }
 
   startButton.addEventListener("click", startGame)
